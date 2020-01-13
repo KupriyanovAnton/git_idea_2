18a5e92c200c69556ef80280c27c2153c6418d31
Anton <58876946+KupriyanovAnton@users.noreply.github.com>
2020-01-13 22:21:02 +0400

    Update Main.java
    github/second brunch_3

--- src/ru/kupriyanov/Main.java
+++ src/ru/kupriyanov/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world______");
         System.out.println("This changinging has been writen via github/second brunch");
+        System.out.println("This changinging has been writen via github/second brunch");
     }
 }
