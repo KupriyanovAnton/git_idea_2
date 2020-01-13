0139f4662b7f87845d7dddc42d98711255d1f5c0
Kuprianov Anton <kuprianov.anton@yandex.ru>
2020-01-13 22:59:25 +0400

    last changes

--- src/ru/kupriyanov/Main.java
+++ src/ru/kupriyanov/Main.java
@@ -2,14 +2,6 @@ package ru.kupriyanov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world______");
-        System.out.println("This changinging has been writen via github/second brunch");
-        System.out.println("This changinging has been writen via github/second brunch");
-        System.out.println("This changinging has been writen via idea/second branch");
-        System.out.println("Hey via idea/second branch");
-
-        int a = 1;
-
-        String str = "asd";
+        System.out.println("asd");
     }
 }
